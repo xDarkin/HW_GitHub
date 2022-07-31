f04de44088d55361db1efba289a72918fea45aad
Darkin <pavel.meleshin@gmail.com>
2022-07-31 15:53:47 +0300

    commit hw lesson_4

---
+++ lesson_4/task_3.py
@@ -0,0 +1,8 @@
+pass1 = "password1"
+pass2 = "password2"
+pass3 = "password3"
+inp_pass = input("Введіть пароль:")
+if inp_pass == pass1 or inp_pass == pass2 or inp_pass == pass3:
+    print("Пароль прийнято")
+else:
+    print("У доступі відмовлено!")
