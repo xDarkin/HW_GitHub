27cdb18b1a3a9b9d2ad47d4451c2a12d8d709fd5
Darkin <pavel.meleshin@gmail.com>
2022-08-07 13:45:20 +0300

    commit hw lesson_5 all tasks done.py

--- lesson_5/task_1.py
+++ lesson_5/task_1.py
@@ -1,9 +1,9 @@
 num = int(input("Input number: "))
 
-while num > 0:
+while num > 1:
     num_1 = num
     a = num % 10
-    while num_1 > 0:
+    while num_1 > 1:
         c = num_1 // 10
         b = c % 10
         if a == b:
