e61f0f97501d0623c92446abc97f8970f930d2ab
Darkin <pavel.meleshin@gmail.com>
2022-08-09 21:42:48 +0300

    commit hw lesson_7 task_3.py

---
+++ lesson_7/task_3.py
@@ -0,0 +1,11 @@
+
+a = list(input("Input first list of numbers: "))
+b = list(input("Input second list of numbers: "))
+count = 0
+a[len(a):] = b
+for i in range(0, len(a)):
+    a[i] = int(a[i])
+for i in range(0, 10):
+    if a.count(i) > 0:
+        count += 1
+print(count)
