faf9043a270511ec13362d2daed71f9a4c177ec9
Darkin <pavel.meleshin@gmail.com>
2022-08-10 19:26:12 +0300

    update hw lesson_7 task_2.py

--- lesson_7/task_2.py
+++ lesson_7/task_2.py
@@ -3,7 +3,6 @@ v = list(input("Input numbers: "))
 k = int(input("Input index: "))
 C = input("Input insertable number: ")
 print(v)
-v[k+1:len(v)] = v[k:len(v)-1]
+v[k+1:len(v)+1] = v[k:len(v)]
 v[k] = C
-v.append("!")
 print(v)
