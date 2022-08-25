448df0c0ceed3406285828914c6f1cbaa7ea6211
Darkin <pavel.meleshin@gmail.com>
2022-08-25 21:47:01 +0300

    update hw lesson_11

--- lesson_11/task_4.py
+++ lesson_11/task_4.py
@@ -1,29 +1,24 @@
-k = {}
-
 
 def table(func):
 
-    def wrapper():
-        func()
-        global k
-        k = list(k)
-        k.sort()
-        print(" ", "_"*len(str(k[-1])), " ", "_"*11, " ", "_"*8)
-        for i in range(len(k)):
-            print("|", str(k[i]).ljust(len(str(k[-1])), " "), "|", "кратне 3".ljust(11, " ") if k[i] % 3 == 0
+    def wrapper(*args, **kwargs):
+        g = func(*args, **kwargs)
+        g = list(g)
+        g.sort()
+        print(" ", "_"*len(str(g[-1])), " ", "_"*11, " ", "_"*8)
+        for i in range(len(g)):
+            print("|", str(g[i]).ljust(len(str(g[-1])), " "), "|", "кратне 3".ljust(11, " ") if g[i] % 3 == 0
                   else "не кратне 3".ljust(11, " "),
-                  "|", "парне".ljust(8, " ") if k[i] % 2 == 0 else "не парне".ljust(8, " "), "|")
-            print("|", "_" * len(str(k[-1])), "|", "_" * 11, "|", "_" * 8, "|")
+                  "|", "парне".ljust(8, " ") if g[i] % 2 == 0 else "не парне".ljust(8, " "), "|")
+            print("|", "_" * len(str(g[-1])), "|", "_" * 11, "|", "_" * 8, "|")
     return wrapper
 
 
 @table
-def numbers():
+def numbers(x):
     import random
-    x = int(input("Input number: "))
     n = set(random.randint(1, 99999) for i in range(x))
-    global k
-    k = n
+    return n
 
 
-numbers()
+numbers(10)
