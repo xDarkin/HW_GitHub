832b0749ef7a64ef5c3276f7ff30cf67bb8caf7c
Darkin <pavel.meleshin@gmail.com>
2022-09-03 20:07:56 +0300

    fix hw lesson_13_1

--- lesson_13_1/task_1.py
+++ lesson_13_1/task_1.py
@@ -3,7 +3,16 @@ import random
 n = int(input("Введіть кількість рядків: "))
 m = int(input("Введіть кількість ствопців: "))
 a = [[random.randint(1, 50) for i in range(m)] for j in range(n)]
-o = [sum([a[i][j] for i in range(n)]) for j in range(m)]
+
+
+def sum_h(i):
+    b = 0
+    for j in range(n):
+        b += a[j][i]
+    return b
+
+
+o = [sum_h(i) for i in range(m)]
 
 
 def sum_v(j):
