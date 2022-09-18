934fe5c544ad0fe0633599ec8883d28efeb72352
Darkin <pavel.meleshin@gmail.com>
2022-09-18 17:55:28 +0300

    commit hw lesson_16_board

---
+++ lesson_16/sea_battle_pack/mod_board.py
@@ -0,0 +1,43 @@
+class BoardClass:
+    board: list = [[0 for i in range(20)] for j in range(10)]
+
+    def board_print(self):
+        a = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"]
+        print("┏━━━" + "┳━━━" * 9 + "┓" + " "*7 + "┏━━━" + "┳━━━" * 9 + "┓")
+        for i in range(10):
+            for j in range(10):
+                print("┃", end="")
+                if self.board[i][j] == 0:
+                    print("", " ", end=" ")
+                elif self.board[i][j] == 1:
+                    print("", ".", end=" ")
+                elif self.board[i][j] == 2:
+                    print("", "▓", end=" ")
+                else:
+                    print("", "X", end=" ")
+            print("┃  ", a[i], end="   ")
+            for j in range(10, 20):
+                print("┃", end="")
+                if board[i][j] == 0:
+                    print("", " ", end=" ")
+                elif board[i][j] == 1:
+                    print("", ".", end=" ")
+                else:
+                    print("", "X", end=" ")
+            print("┃")
+            print("┣━━━╋━━━╋━━━╋━━━╋━━━╋━━━╋━━━╋━━━╋━━━╋━━━┫" + " "*7 + "┣━━━╋━━━╋━━━╋━━━╋━━━╋━━━╋━━━╋━━━╋━━━╋━━━┫")
+        print("┃ 1 ┃ 2 ┃ 3 ┃ 4 ┃ 5 ┃ 6 ┃ 7 ┃ 8 ┃ 9 ┃ 10┃" + " "*7 + "┃ 1 ┃ 2 ┃ 3 ┃ 4 ┃ 5 ┃ 6 ┃ 7 ┃ 8 ┃ 9 ┃ 10┃")
+        print("┗━━━" + "┻━━━" * 9 + "┛" + " "*7 + "┗━━━" + "┻━━━" * 9 + "┛")
+
+    def __getitem__(self, key):
+        return self.board[key]
+
+
+if __name__ == "__main__":
+    board = BoardClass()
+    board[0][0] = 1
+    board[1][1] = 2
+    board[2][2] = 3
+    board[0][10] = 3
+    board[9][19] = 3
+    board.board_print()
